5f4bb3dd135c60a451746b72b37e9f6975e309b0
68994510+pkakria@users.noreply.github.com <68994510+pkakria@users.noreply.github.com>
2021-05-03 13:42:09 -0700

    removed address, accountNo from Tables

--- application.py
+++ application.py
@@ -22,11 +22,11 @@ def create_db():
     cur.execute("DROP TABLE IF EXISTS Accounts")
     cur.execute("PRAGMA foreign_keys = ON")
     cur.execute("CREATE TABLE IF NOT EXISTS Users(userId INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT, email TEXT, lastname TEXT, firstname TEXT, address TEXT)")
-    cur.execute("CREATE TABLE IF NOT EXISTS Accounts(accountId INTEGER PRIMARY KEY AUTOINCREMENT, AccountNo INTEGER UNIQUE, currentBalance REAL, userId INTEGER, FOREIGN KEY(userId) REFERENCES Users(userId))")
+    cur.execute("CREATE TABLE IF NOT EXISTS Accounts(accountId INTEGER PRIMARY KEY AUTOINCREMENT, currentBalance REAL, userId INTEGER, FOREIGN KEY(userId) REFERENCES Users(userId))")
     #insert a new user with auto-assigned userId=1
-    cur.execute('INSERT INTO Users(username, password, email, lastname, firstname, address) VALUES(?, ?, ?, ?, ?, ?)', ( 'testuser', '012345', 'test@gmail.com', 'testlastname', 'testfirstname', '1000 Palo Verde Road, Irvine'))
-    # insert an account for user with userId=1
-    cur.execute('INSERT INTO Accounts(AccountNo, currentBalance, userId) VALUES(?, ?, ?)', (2222222222, 5000, 1))
+    cur.execute('INSERT INTO Users(username, password, email, lastname, firstname) VALUES(?, ?, ?, ?, ?)', ( 'testuser', '012345', 'test@gmail.com', 'testlastname', 'testfirstname'))
+    # insert an account for user with accountId = 1 auto-assigned and userId=1
+    cur.execute('INSERT INTO Accounts(currentBalance, userId) VALUES(?, ?)', (5000, 1))
 
     conn.commit()
     cur.close()
