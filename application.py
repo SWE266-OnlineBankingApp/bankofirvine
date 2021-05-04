9947e3ae08c0d3a712c7208e0c9390c0d79aa890
smk-dev20 <smk.dev20@gmail.com>
2021-05-03 17:36:45 -0700

    updates for Task 2 and 3

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -8,24 +8,43 @@ def home():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        print(username, password)
-        #code to check against DB 
-        #assuming invalid
-        feedback = f"Username or Password is invalid"
-        return render_template('home.html',feedback=feedback)
-       
-        # assuming correct
-        #return redirect('account')
+         
+        # validate entered username and password strings 
 
+        #code to check against DB
+        conn = sqlite3.connect('bankdata.db')
+        cur = conn.cursor()   
+        userid = cur.execute("select userId from Users where username= ? and password = ? ",(username, password)).fetchone()  
+        conn.close()
+   
+        if(not userid):
+            feedback = f"Username or Password is invalid"
+            return render_template('home.html',feedback=feedback)
+        else:        
+            return redirect(url_for('account',userid=userid[0]))
+                   
     return render_template('home.html')
 
 @app.route('/register')
 def register():
     return render_template('register.html')    
 
-@app.route('/account')
-def account():
-    return render_template('account.html')   
+@app.route('/account/<userid>')
+def account(userid):
+    #userid will be encoded needs to be decoded first
+
+    conn = sqlite3.connect('bankdata.db')
+    cur = conn.cursor()    
+    name = cur.execute("select name from Users where userid= ? ",[userid]).fetchone()
+    balance = cur.execute("select currentBalance from Accounts where userid=? ",[userid]).fetchone()
+    conn.close()
+
+    if(name == None or balance == None):
+        feedback = f"Something went wrong please login again"
+        return render_template('home.html',feedback = feedback)
+    
+    return render_template('account.html', name = name[0], balance = balance[0])   
+
 
 def create_db():
     conn = sqlite3.connect('bankdata.db')
