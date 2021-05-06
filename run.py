893c6c592b1c55b7f75e08c8ff353743a0fc6085
pkakria <68994510+pkakria@users.noreply.github.com>
2021-05-06 01:23:37 -0700

    random user id

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, session
 from application.functions.util import create_salt, hash_password, validate_str
+import uuid
 
 app = Flask(__name__, template_folder='./application/templates')
 app.config['SECRET_KEY'] = 'bQ9J84inbtoUcjLZFbQTVg'
@@ -64,30 +65,30 @@ def register():
         initial_balance = request.form.get("ibalance", type= float)
         name = request.form.get("name")
         
-        
+        userid = create_random_userid()
         salt = create_salt()
         password_hash = hash_password(password, salt)
         try:
-            create_user(username, password_hash, salt, name)
-            app.logger.debug("username={}, password_hash={}, initial_balance={}, name={}".format(username, password_hash, initial_balance, name))
+            create_user(userid, username, password_hash, salt, name)
+            app.logger.debug("Created user with userId = {}, username={}, password_hash={}, initial_balance={}, name={}".format(userid, username, password_hash, initial_balance, name))
         except sqlite3.IntegrityError as e:
             # uniqu constraint failed. send user back to login page with user already exists
             feedback = "User already exists."
             return render_template('home.html',feedback=feedback)
         else:
-            # user created. get userid for account creation
-            userid = get_userid_from_username(username)
-            app.logger.debug("created userid = {}".format(userid))
+            # # user created. get userid for account creation
+            # userid = get_userid_from_username(username)
+            # app.logger.debug("created userid = {}".format(userid))
             # userid should always be present since we just successfully created a user. nonetheless let's check if a userid was returned by the DB
-            if (userid):
-                create_account(initial_balance, userid)
-                app.logger.debug("New Registration completed")
-                feedback = f"Successful registration. Please login"
-                return render_template('home.html',feedback=feedback)
-            else:
-                app.logger.error("Couldn't locate userid of username= {}".format(username))
-                feedback = f"Couldn't register. Please contact the administrator"
-                return render_template('home.html',feedback=feedback)
+            # if (userid):
+            create_account(initial_balance, userid)
+            app.logger.debug("New Registration completed")
+            feedback = f"Successful registration. Please login"
+            return render_template('home.html',feedback=feedback)
+            # else:
+            #     app.logger.error("Couldn't locate userid of username= {}".format(username))
+            #     feedback = f"Couldn't register. Please contact the administrator"
+            #     return render_template('home.html',feedback=feedback)
     else:
         return render_template('register.html') 
 
@@ -98,7 +99,7 @@ def account():
 
         app.logger.debug("Account access by {}".format(userid))
         conn = sqlite3.connect('bankdata.db')
-        cur = conn.cursor()    
+        cur = conn.cursor()
         name = cur.execute("select name from Users where userId= ? ",[userid]).fetchone()
         balance = cur.execute("select currentBalance from Accounts where userid=? ",[userid]).fetchone()
         conn.close()
@@ -113,11 +114,11 @@ def account():
         app.logger.error("Session not set when accessing account")
         return redirect(url_for('home'))   
 
-def create_user(username, password_hash, salt, name):
+def create_user(userid, username, password_hash, salt, name):
     conn = sqlite3.connect('bankdata.db')
     cur = conn.cursor()
     try:
-        cur.execute("INSERT INTO Users(username, password, salt, name) VALUES(?, ?, ?, ?)", (username, password_hash, salt, name))
+        cur.execute("INSERT INTO Users(userId, username, password, salt, name) VALUES(?, ?, ?, ?, ?)", (userid, username, password_hash, salt, name))
     except sqlite3.IntegrityError as e:
         raise sqlite3.IntegrityError
     finally:
@@ -140,6 +141,9 @@ def create_account(initial_balance, userid):
     conn.commit()
     cur.close()
 
+def create_random_userid():
+    return str(uuid.uuid4());
+
 def create_db():
     app.logger.debug("Initiate DB creation")
     conn = sqlite3.connect('bankdata.db')
@@ -147,15 +151,16 @@ def create_db():
     cur.execute("DROP TABLE IF EXISTS Users")
     cur.execute("DROP TABLE IF EXISTS Accounts")
     cur.execute("PRAGMA foreign_keys = ON")
-    cur.execute("CREATE TABLE IF NOT EXISTS Users(userId INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT, salt TEXT, name TEXT)")
-    cur.execute("CREATE TABLE IF NOT EXISTS Accounts(accountId INTEGER PRIMARY KEY AUTOINCREMENT, currentBalance REAL, userId INTEGER, FOREIGN KEY(userId) REFERENCES Users(userId))")
+    cur.execute("CREATE TABLE IF NOT EXISTS Users(userId TEXT PRIMARY KEY, username TEXT UNIQUE, password TEXT, salt TEXT, name TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Accounts(accountId INTEGER PRIMARY KEY AUTOINCREMENT, currentBalance REAL, userId TEXT, FOREIGN KEY(userId) REFERENCES Users(userId))")
     #insert a new user with auto-assigned userId=1
+    userid = create_random_userid()
     password1 = '012345'
     salt1 = create_salt()
     password_hash = hash_password(password1,salt1)
-    cur.execute('INSERT INTO Users(username, password, salt, name) VALUES(?, ?, ?, ?)', ( 'testuser', password_hash, salt1, 'testname'))
+    cur.execute('INSERT INTO Users(userId, username, password, salt, name) VALUES(?, ?, ?, ?, ?)', (userid, 'testuser', password_hash, salt1, 'testname'))
     # insert an account for user with accountId = 1 auto-assigned and userId=1
-    cur.execute('INSERT INTO Accounts(currentBalance, userId) VALUES(?, ?)', (5000, 1))
+    cur.execute('INSERT INTO Accounts(currentBalance, userId) VALUES(?, ?)', (5000, userid))
 
     conn.commit()
     cur.close()
