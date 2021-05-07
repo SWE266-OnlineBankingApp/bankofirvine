389a8f39923ffc36cdde455afdf3940ab65e469f
smk-dev20 <smk.dev20@gmail.com>
2021-05-07 15:31:23 -0700

    clean

--- run.py
+++ run.py
@@ -1,12 +1,5 @@
-#import sqlite3
-#from flask import Flask, render_template, request, redirect, url_for, session
-
 from application import app
 from application.functions.data_access import create_db
-
-#app = Flask(__name__, template_folder='./application/templates')
-#app.config['SECRET_KEY'] = 'bQ9J84inbtoUcjLZFbQTVg'
-
 
 # run the app.
 if __name__ == "__main__":
