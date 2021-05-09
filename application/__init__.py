8a375d7fc3061f94f78e610e549c8cca0ad3fe14
smk-dev20 <smk.dev20@gmail.com>
2021-05-09 00:35:10 -0700

    fix prod db issue

--- application/__init__.py
+++ application/__init__.py
@@ -4,3 +4,6 @@ app = Flask(__name__, template_folder='./templates')
 app.config['SECRET_KEY'] = 'bQ9J84inbtoUcjLZFbQTVg'
 
 from application import views
+from application.functions.data_access import create_db
+
+create_db()
--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 from application import app
-from application.functions.data_access import create_db
+
 
 # run the app.
 if __name__ == "__main__":
     app.debug = True
-    create_db()
     app.run()
     
 
