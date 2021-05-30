baae73344490580c346f1cb501e9d145a9b53821
68994510+pkakria@users.noreply.github.com <68994510+pkakria@users.noreply.github.com>
2021-05-30 15:48:20 -0700

    removed unused talisman comment

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
-#from flask_talisman import Talisman
 from datetime import timedelta
 
 app = Flask(__name__, template_folder='./templates')
-#Talisman(app)
 from application import views
 from application.functions.data_access import create_db
 from application.functions.util import gen_key
